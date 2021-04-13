44b05243d714c15485a23a203403bce7c9433078
RafaRibeiroRodri <rafael.rodrigues66@fatec.sp.gov.br>
2021-04-12 22:55:10 -0300

    criando o backend

--- src/api/app.js
+++ src/api/app.js
@@ -9,7 +9,17 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/app', rotaDados);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Header", "PUT, POST, DELETE, GET");   
+    }
+    next();
+})
+
+app.use('/', rotaDados);
 
 
 module.exports = app;
--- src/api/routes/morador.js
+++ src/api/routes/morador.js
@@ -1,37 +1,101 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').connection;
 
-router.get('/', (req, res) => {
-    res.status(200).send({
-        message: "rota de dados pessoais"
-    });
+//post de moradores
+router.post('/morador/registro', (req, res) => {
+    console.log(req.body);
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'INSERT INTO morador (email, senha, nome, cpf, rg, data_nasc, unidade, bloco, telefone, veic_placa, veic_modelo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [req.body.email, req.body.senha, req.body.nome, req.body.cpf, req.body.rg, req.body.data_nasc, req.body.unidade, req.body.bloco, req.body.telefone, req.body.placa, req.body.modelo],
+            (error, resultado, field) => {
+                conn.release();
+                if(error) { return res.status(500).send({ error: error, response: null }); }
+                res.status(200).send({
+                    message: "Usuário criado com sucesso",
+                    morador_id: resultado.insertId
+                });
+            }
+        )        
+    })
 });
 
-router.post('/', (req, res) => {
-    const httpreq = {
-        email: req.body.email,
-        senha: req.body.senha
-    }
-    res.status(201).send({
-        message: "rota de login",
-        morador: httpreq
-    });
+//get de moradores
+router.get('/morador/dados', (req, res) => {
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'SELECT * FROM morador',
+            (error, resultado, field) => {
+                console.log("resultado", resultado);
+                conn.release();
+                if(error) { return res.status(500).send({ error: error, response: null }); }
+                res.status(200).send({
+                    morador: resultado
+                });
+            }
+        )        
+    })
 });
 
-router.get('/:morador_id', (req, res) => {
-    const id = req.params.morador_id
+//get de moradores individuais
 
-    if(id === "teste") {
-        res.status(200).send({
-            message: "rota get com id de teste",
-            id: id
-        });
-    } else {
-        res.status(200).send({
-            message: "rota get com id",
-            id: id
-        });
-    }
+
+//post de noticias para o mural
+router.post('/noticias/registro', (req, res) => {
+    console.log(req.body);
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'INSERT INTO noticias (conteudo) VALUES (?)',
+            [req.body.conteudo],
+            (error, resultado, field) => {
+                conn.release();
+                if(error) { return res.status(500).send({ error: error, response: null }); }
+                res.status(200).send({
+                    message: "Noticía inserida com sucesso",
+                    morador_id: resultado.insertId
+                });
+            }
+        )        
+    })
 });
+
+//get de noticias para o mural
+router.get('/noticias', (req, res) => {
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'SELECT * FROM noticias',
+            (error, resultado, field) => {
+                console.log("resultado", resultado);
+                conn.release();
+                if(error) { return res.status(500).send({ error: error, response: null }); }
+                res.status(200).send({
+                    morador: resultado
+                });
+            }
+        )        
+    })
+});
+
+//post de reclamação/sugestão para o suporte
+router.post('/suporte', (req, res) => {
+    console.log(req.body);
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'INSERT INTO suporte (nome, unidade, bloco, telefone, texto) VALUES (?, ?, ?, ?, ?)',
+            [req.body.nome, req.body.unidade, req.body.bloco, req.body.telefone, req.body.texto],
+            (error, resultado, field) => {
+                conn.release();
+                if(error) { return res.status(500).send({ error: error, response: null }); }
+                res.status(200).send({
+                    message: "Sua reclamação/sugestão foi enviada ao suporte",
+                    morador_id: resultado.insertId
+                });
+            }
+        )        
+    })
+});
+
+
 
 module.exports = router;
