a0cf15223000b8871ce4533b58e50d86fcede380
RafaRibeiroRodri <rafael.rodrigues66@fatec.sp.gov.br>
2021-06-14 22:40:55 -0300

    arrumando tela

--- src/app/dadospessoais/dados.page.ts
+++ src/app/dadospessoais/dados.page.ts
@@ -50,22 +50,17 @@ export class DadosPage {
         bloco: usuario.bloco,
         telefone: usuario.telefone
       };
-      this.foto = JSON.parse(localStorage.getItem('foto')).foto; // quando vc implementar o campo foto no tabela de usuários, é só atribuir o valor pra essa variável que já vai trocar no html
+      this.foto = JSON.parse(localStorage.getItem('foto')).foto;
     });
   }
 
   async openCamera() {
     try {
-      // aqui eu chamo a promise que abre a câmera do celular e pego a resposta na variável imageData pra depois salvar na currentImage
       const imageData = await this.camera.getPicture({
         quality: 100,
-        // aqui eu defino o tipo do dado, no caso, quero a base64 da imagem
         destinationType: this.camera.DestinationType.DATA_URL,
-        // formato da imagem
         encodingType: this.camera.EncodingType.JPEG,
-        // aqui é o tipo da midia, no caso, eu quero só imagem
         mediaType: this.camera.MediaType.PICTURE,
-        // aqui é a orientação da imagem, se n colocar true, ela vem em modo paisagem
         correctOrientation: true
         }
       );
